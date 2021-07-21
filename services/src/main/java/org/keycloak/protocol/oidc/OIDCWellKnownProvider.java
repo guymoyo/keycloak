3d754a3e9d543d017250733cebb294598a40bc02
gmo <Marguerite1960>
2021-07-21 13:53:03 +0200

    review

--- services/src/main/java/org/keycloak/protocol/oidc/OIDCWellKnownProvider.java
+++ services/src/main/java/org/keycloak/protocol/oidc/OIDCWellKnownProvider.java
@@ -59,6 +59,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Collections;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
